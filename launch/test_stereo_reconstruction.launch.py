736054b5b4472e7023c95e2689c3729a3a3f95a2
marcolapo5 <marco.lapolla5@gmail.com>
2022-08-09 15:18:33 +0200

    Update

--- launch/test_stereo_reconstruction.launch.py
+++ launch/test_stereo_reconstruction.launch.py
@@ -8,35 +8,10 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    params_allied_r_filepath = os.path.join(
-        get_package_share_directory("stereo_reconstruction_cpp"),
-        "params",
-        "params_cam_right.yaml",
-    )
-    params_allied_l_filepath = os.path.join(
-        get_package_share_directory("stereo_reconstruction_cpp"),
-        "params",
-        "params_cam_left.yaml",
-    )
-
     params_stereo_filepath = os.path.join(
         get_package_share_directory("stereo_reconstruction_cpp"),
         "params",
         "params_stereo_reconstruction_tests.yaml",
-    )
-
-    node_cam_right = Node(
-        package="hal_allied_vision_camera",
-        executable="av_node",
-        name="av_node_right",
-        parameters=[params_allied_r_filepath],
-    )
-
-    node_cam_left = Node(
-        package="hal_allied_vision_camera",
-        executable="av_node",
-        name="av_node_left",
-        parameters=[params_allied_l_filepath],
     )
 
     stereo_acquisition_node = Node(
@@ -48,7 +23,5 @@ def generate_launch_description():
     
 
     return LaunchDescription([
-        node_cam_right, 
-        node_cam_left, 
         stereo_acquisition_node
     ])
